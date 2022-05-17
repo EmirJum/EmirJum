9923449c1b2d0348e02d655682af06504ae84ae8
EmirJum <mirkajum@gmail.com>
2022-05-17 22:39:26 +0600

    dz2

---
+++ lesson2/index.js
@@ -0,0 +1,61 @@
+// function a() {
+//   var b = "ok";
+// }
+
+
+// console.log(b);
+// const init = () => {
+// 	let name = 'name';
+// 	const displayName = () => {
+// 		console.log(name)
+// 	};
+// 	displayName();
+// }
+// init();\
+
+
+
+// const hi = () => {
+// 	console.log("hi");
+// };
+// setTimeout(hi, 2000);
+
+// console.log("ok");
+// setTimeout(()=> {
+// 	console.log("ok");
+// }, 2000);
+
+// setInterval(() => {
+// 	console.log('hi');
+// }, 3000);
+// const box = document.querySelector('.box');
+
+// const block = document.querySelector('.block');
+
+// let position = 0;
+// let topPosition = 0;
+
+// const move = () => {
+// 	if (position < 350){
+// 		position += 16;
+// 		block.style.left = `${position}px`;
+// 		setTimeout(() => {
+// 			move();
+// 		}, 200);
+// 	}else if(position >= 350){
+//     topPosition += 16;
+//     // block.style.top = `${topPosition}px`;
+//     setTimeout(() => {
+// 			move();
+// 		}, 200);
+//   }
+// };
+
+// move();
+
+// const interval = setInterval(hi, 2000);
+
+// setTimeout(() => {
+// 	clearInterval(interval);
+// }, 6000);
+
