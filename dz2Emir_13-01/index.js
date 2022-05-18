0f58a999bba18e4435ccdd823b3fe6dfcf724ce9
EmirJum <mirkajum@gmail.com>
2022-05-18 17:44:28 +0600

    dz3

--- dz2Emir_13-01/index.js
+++ dz2Emir_13-01/index.js
@@ -4,31 +4,7 @@ const box2 = document.querySelector(".box2");
 let yPos = 0;
 let xPos = 0;
 
-function move() {
-	if(xPos < 340 && yPos == 0) {
-		xPos += 16;
-		box2.style.left = `${xPos}px`;
-		setTimeout(() => {
-			move();
-		}, 200);
-	}  else if (xPos >= 340 && yPos < 340 ) {
-		yPos += 16;
-		box2.style.top = `${yPos}px`;
-		setTimeout(() => {
-			move();
-		}, 200);
-	}  else if (xPos > 0 && yPos >= 340) {
-		xPos -= 16;
-		box2.style.left = `${xPos}px`;
-		setTimeout(() => {
-			move();
-		}, 200)
-	} else if (xPos == 0 && yPos > 0) {
-		yPos -= 16;
-		box2.style.top = `${yPos}px`;
-		setTimeout(() => {
-			move();
-		}, 200)
-	}
+function move () {
+  xPos += 16;
+  box2.style.left = `$`
 }
-move()
